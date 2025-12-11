9203c1b32a93906ed5f678920de7880da21d5463
Nay Nyein Chan <naychan700@gmail.com>
2025-12-11 12:28:54 +0700

    refactor: update dispatching methods to deprecate string class name usage

--- src/Bus/ServesFeature.php
+++ src/Bus/ServesFeature.php
@@ -3,7 +3,16 @@
 namespace Laranex\BetterLaravel\Bus;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Laranex\BetterLaravel\Cores\Feature;
 
+/**
+ * Trait ServesFeature
+ *
+ * Provides functionality to serve features synchronously.
+ * This trait combines the Dispatcher and DispatchesJobs traits to enable
+ * synchronous execution of feature classes within the Better Laravel architecture.
+ *
+ */
 trait ServesFeature
 {
     use Dispatcher, DispatchesJobs;
@@ -11,9 +20,17 @@ trait ServesFeature
     /**
      * Serve the given feature with the given arguments.
      *
-     * @param  string  $feature
+     * Dispatches a feature synchronously and returns its result. The feature can be
+     * provided as either a class name string or an instantiated object. Arguments
+     * are passed to the feature's constructor if a class name is provided.
+     *
+     * @param  string|Feature  $feature  The feature to serve - either a fully qualified class name or an instance
+     * @param  array $arguments  The arguments to pass to the feature's constructor (only used if $feature is a class name)
+     * @return mixed  The result returned by the feature's execution
+     *
+     * @deprecated Passing a string class name to serve method is deprecated and will be removed in a future version. Please instantiate the unit directly: use `new YourFeature()` instead of `YourFeature::class`.
      */
-    public function serve(mixed $feature, array $arguments = []): mixed
+    public function serve(string|Feature $feature, array $arguments = []): mixed
     {
         return $this->dispatchSync($this->getDispatchableUnit($feature, $arguments));
     }
--- src/Bus/UnitDispatcher.php
+++ src/Bus/UnitDispatcher.php
@@ -4,17 +4,33 @@ namespace Laranex\BetterLaravel\Bus;
 
 use Error;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Laranex\BetterLaravel\Cores\Job;
 use Laranex\BetterLaravel\Cores\Operation;
 use Laranex\BetterLaravel\Cores\QueueableJob;
 
+/**
+ * Trait UnitDispatcher
+ *
+ * Provides functionality to dispatch units (Jobs, Operations) synchronously or to queues.
+ * This trait combines the Dispatcher and DispatchesJobs traits to offer a unified interface
+ * for running units with or without queue support.
+ *
+ */
 trait UnitDispatcher
 {
     use Dispatcher, DispatchesJobs;
 
     /**
-     * Dispatch the given unit with the given arguments.
+     * Dispatch the given unit synchronously with the provided arguments.
      *
-     * @param  string  $unit
+     * This method will run the unit immediately in the current process and return the result.
+     * The unit can be either a class name (string) or an instance of a Job/Operation.
+     *
+     * @param  string|Job|Operation  $unit  The unit to dispatch (class name or instance)
+     * @param  array<string, mixed>  $arguments  Arguments to pass to the unit's constructor
+     * @return mixed  The result returned by the unit's execution
+     *
+     * @deprecated Passing a string class name to run method is deprecated and will be removed in a future version. Please instantiate the unit directly: use `new YourJob()` instead of `YourJob::class`.
      */
     public function run(mixed $unit, array $arguments = []): mixed
     {
@@ -22,13 +38,22 @@ trait UnitDispatcher
     }
 
     /**
-     * Serve the given unit with arguments in given queue.
+     * Dispatch the given unit to a queue with the provided arguments.
      *
-     * @param  string  $unit
+     * This method will queue the unit for asynchronous execution. The unit must be queueable
+     * (extend QueueableJob). Operations are not yet supported for queueing and will throw an error.
      *
-     * @throws Error
+     * @param  string|Job|Operation  $unit  The unit to dispatch (class name or instance, must be queueable)
+     * @param  array<string, mixed>  $arguments  Arguments to pass to the unit's constructor
+     * @param  string  $queue  The queue name to dispatch the unit to (defaults to 'default')
+     * @return mixed  The result of the dispatch operation
+     *
+     * @throws Error  If the unit does not support queues (must extend QueueableJob)
+     * @throws Error  If the unit is an Operation (not yet supported for queueing)
+     *
+     * @deprecated Passing a string class name to runInQueue method is deprecated and will be removed in a future version. Please instantiate the unit directly: use `new YourJob()` instead of `YourJob::class`.
      */
-    public function runInQueue(mixed $unit, array $arguments = [], string $queue = 'default'): mixed
+    public function runInQueue(string|Job|Operation $unit, array $arguments = [], string $queue = 'default'): mixed
     {
         $dispatchableUnit = $this->getDispatchableUnit($unit, $arguments);
 
