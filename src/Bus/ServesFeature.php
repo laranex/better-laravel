066be777125421cf13ae845c878b9cb158c26e95
Nay Nyein Chan <naychan700@gmail.com>
2026-03-25 11:22:28 +0700

    fix: github test workflow

--- src/Bus/ServesFeature.php
+++ src/Bus/ServesFeature.php
@@ -25,7 +25,6 @@ trait ServesFeature
      *
      * @param  Feature  $feature  The feature to serve - either a fully qualified class name or an instance
      * @return mixed The result returned by the feature's execution
-     *
      */
     public function serve(Feature $feature): mixed
     {
